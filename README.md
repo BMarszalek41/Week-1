7191ced223b565bffc2b918ae364cbc884d064b2
Bob Marszalek <bobmarszalek@bobs-mbp.lan>
2023-02-03 13:53:28 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+My First Front End Coding Assignment
+
+This is my first attempt at coding
---
+++ index.js
@@ -0,0 +1,32 @@
+var bankAccountBalance = 712.32;
+
+var firstName = 'Bob';
+var lastName = 'Marszalek';
+
+var myWife = 'Megan';
+
+var howManyKids = 2;
+
+var favoriteNumber = 41;
+
+var itIsColdOut = true;
+
+console.log('I have a lovely bunch of Coconuts');
+
+console.log('I hope this works right');
+
+console.log(71 + 5);
+
+console.log(42 - 16);
+
+console.log(21 * 3);
+
+console.log(16 / 2);
+
+console.log(bankAccountBalance + 100);
+
+console.log(myWife + firstName + lastName);
+
+console.log(23 == 23);
+
+console.log(23 == 22);
